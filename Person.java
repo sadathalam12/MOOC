27a3df8dc1092b29c31a378adaa75e4a01371b66
sadathalam12 <47162665+sadathalam12@users.noreply.github.com>
2019-08-09 02:11:23 -0400

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,45 @@
+import java.util.Calendar;
+
+public class Person {
+    private String name;
+    private MyDate birthday;
+   
+   
+    
+    public Person(String name, int day, int month, int year) {
+        this.name = name;
+        this.birthday = new MyDate(day, month, year);
+        
+    }
+   
+    
+    
+    public int age() {
+// calculate the age based on the birthday and the current day
+        // you get the current day as follows: 
+        int currentDate = Calendar.getInstance().get(Calendar.DATE);
+        int currentMonth= Calendar.getInstance().get(Calendar.MONTH) + 1; 
+        int currentYear= Calendar.getInstance().get(Calendar.YEAR);
+        MyDate dateNow = new MyDate(currentDate,currentMonth,currentYear);
+      int age =  dateNow.differenceInYears(birthday);
+        return age;
+    }
+    
+    public boolean olderThan(Person compared) {
+        int age1 = this.age(); 
+        int age2 = compared.age(); 
+       int differenceOfAge= age1-age2; 
+       if(differenceOfAge > 0){
+           return true; 
+       }else 
+           return false; 
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+    
+    public String toString() {
+        return this.name + ", born " + this.birthday;
+    }
+}
