8fe6b3abd245d29f9223011d4bd997cacf5fa155
sadathalam12 <47162665+sadathalam12@users.noreply.github.com>
2019-08-13 20:48:07 -0400

    Add files via upload

---
+++ Phonebook.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sadat
+ */
+import java.util.ArrayList;
+public class Phonebook {
+    private ArrayList<Person> people = new ArrayList<Person>(); 
+     
+    
+    
+    public void add(String name, String phoneNumber ){
+      Person p = new Person(name, phoneNumber); 
+        people.add(p); 
+       
+        
+    }
+    public void printAll(){
+       
+        for(int i=0; i<people.size();i++){
+            System.out.println(people.get(i));
+        }
+    }
+    public String searchNumber(String name){
+       int i = 0; 
+       String a = null;  
+       Person x; 
+      
+       while(i<people.size()){
+          x= people.get(i); 
+          
+           
+           if(x.getName()==name){
+               a = x.getNumber(); 
+           }else{
+              a= "not known number"; 
+           }
+          i++; 
+           
+       }
+       
+        return a; 
+      }
+         
+             
+         }
+        
+    
+    
+
