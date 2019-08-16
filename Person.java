5ef517ef55341662fd93e72fca2ebd28a619abe4
MVR <28614088+mchlvncntry@users.noreply.github.com>
2019-08-16 01:41:59 -0700

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,38 +1,32 @@
 import java.util.Calendar;
 
 public class Person {
+    
     private String name;
     private MyDate birthday;
    
-   
-    
     public Person(String name, int day, int month, int year) {
         this.name = name;
-        this.birthday = new MyDate(day, month, year);
-        
+        this.birthday = new MyDate(day, month, year);     
     }
-   
-    
     
     public int age() {
-// calculate the age based on the birthday and the current day
-        // you get the current day as follows: 
+    // calculate the age based on the birthday and the current day
+    // you get the current day as follows: 
         int currentDate = Calendar.getInstance().get(Calendar.DATE);
         int currentMonth= Calendar.getInstance().get(Calendar.MONTH) + 1; 
         int currentYear= Calendar.getInstance().get(Calendar.YEAR);
         MyDate dateNow = new MyDate(currentDate,currentMonth,currentYear);
-      int age =  dateNow.differenceInYears(birthday);
-        return age;
+        
+        return dateNow.differenceInYears(birthday);
     }
     
     public boolean olderThan(Person compared) {
         int age1 = this.age(); 
         int age2 = compared.age(); 
-       int differenceOfAge= age1-age2; 
-       if(differenceOfAge > 0){
-           return true; 
-       }else 
-           return false; 
+        int differenceOfAge= age1-age2; 
+        
+        return differenceOfAge > 0;
     }
     
     public String getName() {
